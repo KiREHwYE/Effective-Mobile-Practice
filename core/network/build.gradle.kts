bf70c048b10e7c777ea532a9d8986759f0864ee5
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 17:46:17 +0300

    feat: добавлен Ktor для работы с HTTP-запросами

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -22,4 +22,12 @@ dependencies {
     // Dagger
     implementation(libs.dagger)
     kapt(libs.google.dagger.compiler)
+
+    //Ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.logging)
 }
