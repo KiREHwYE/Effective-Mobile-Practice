78f50e8f6307ba75e601a644dd492094c33bbe34
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 16:43:08 +0300

    feat: добавлена зависимость для сериализации и удалён лишний файл в модуле Network

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
     id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    kotlin("jvm") version "1.9.0"
+    // Сериализация
+    kotlin("plugin.serialization") version "1.9.0"
 }
+
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    // Сериализация
+    implementation(libs.kotlinx.serialization.json)
 }
--- core/network/src/main/java/com/kire/network/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.kire.network
-
-class MyClass {
-}
