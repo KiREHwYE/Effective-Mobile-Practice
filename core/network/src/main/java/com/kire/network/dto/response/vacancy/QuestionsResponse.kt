0ba4ea0b6a2bf7694c0dc3e0ae0d60b8af889fbe
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 18:06:16 +0300

    refactor: добавлено описание к QuestionsResponse

--- core/network/src/main/java/com/kire/network/dto/response/vacancy/QuestionsResponse.kt
+++ core/network/src/main/java/com/kire/network/dto/response/vacancy/QuestionsResponse.kt
@@ -2,6 +2,11 @@ package com.kire.network.dto.response.vacancy
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Содержит вопросы, которые может задать пользователь
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 @Serializable
 data class QuestionsResponse(
     val questions: List<String> = emptyList()
