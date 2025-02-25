ae036705f3c30630fb364487cb4c3aaa5a09e28a
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 18:02:12 +0300

    refactor: добавлено описание к OfferResponse

--- core/network/src/main/java/com/kire/network/dto/response/offer/OfferResponse.kt
+++ core/network/src/main/java/com/kire/network/dto/response/offer/OfferResponse.kt
@@ -2,6 +2,11 @@ package com.kire.network.dto.response.offer
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Ответ API для offer. Содержит предложения/идеи для пользователя
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 @Serializable
 data class OfferResponse(
     val id: String? = null,
