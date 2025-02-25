949e3018c196a5c09e051d57a1d15178bb20a8c9
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 17:24:25 +0300

    feat: добавил object с ссылками (ссылкой на мок) для получения данных

---
+++ core/network/src/main/java/com/kire/network/dto/response/HttpRoutes.kt
@@ -0,0 +1,6 @@
+package com.kire.network.dto.response
+
+object HttpRoutes {
+    val BASE_URL: String
+        get() = "https://drive.usercontent.google.com/u/0/uc?id=1z4TbeDkbfXkvgpoJprXbN85uCcD7f00r&export=download"
+}
