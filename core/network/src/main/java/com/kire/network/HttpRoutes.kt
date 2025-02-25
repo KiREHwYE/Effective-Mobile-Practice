920f83f2c524d1122a290af96e339d91cf6f9efb
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 17:58:25 +0300

    refactor: добавлен модификатор internal к HttpRoutes

--- core/network/src/main/java/com/kire/network/HttpRoutes.kt
+++ core/network/src/main/java/com/kire/network/HttpRoutes.kt
@@ -1,6 +1,6 @@
 package com.kire.network
 
-object HttpRoutes {
+internal object HttpRoutes {
     val BASE_URL: String
         get() = "https://drive.usercontent.google.com/u/0/uc?id=1z4TbeDkbfXkvgpoJprXbN85uCcD7f00r&export=download"
 }
