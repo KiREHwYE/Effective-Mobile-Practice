4e91df2f9dfbab690ba575cd485df8a2db1e16ce
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 18:07:48 +0300

    refactor: добавлено описание к SchedulesResponse

--- core/network/src/main/java/com/kire/network/dto/response/vacancy/SchedulesResponse.kt
+++ core/network/src/main/java/com/kire/network/dto/response/vacancy/SchedulesResponse.kt
@@ -2,6 +2,12 @@ package com.kire.network.dto.response.vacancy
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Содержит информацию о времени занятости
+ * в рамках предлагаемой работы
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 @Serializable
 data class SchedulesResponse(
     val schedules: List<String> = emptyList()
