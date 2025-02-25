3b96486b5bf51c6647902c5948219cb1ffc0ca80
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 18:04:35 +0300

    refactor: изменено описание ButtonResponse

--- core/network/src/main/java/com/kire/network/dto/response/offer/ButtonResponse.kt
+++ core/network/src/main/java/com/kire/network/dto/response/offer/ButtonResponse.kt
@@ -3,7 +3,7 @@ package com.kire.network.dto.response.offer
 import kotlinx.serialization.Serializable
 
 /**
- * Ответ API для поля button в offer. Содержит текст кнопки
+ * Содержит текст кнопки
  *
  * @author Михаил Гонтарев (KiREHwYE)
  */
