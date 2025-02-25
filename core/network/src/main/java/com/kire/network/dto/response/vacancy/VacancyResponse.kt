7280ea39663a86a672c2c4467883ef23232dab4d
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 18:08:11 +0300

    refactor: добавлено описание к VacancyResponse

--- core/network/src/main/java/com/kire/network/dto/response/vacancy/VacancyResponse.kt
+++ core/network/src/main/java/com/kire/network/dto/response/vacancy/VacancyResponse.kt
@@ -2,6 +2,11 @@ package com.kire.network.dto.response.vacancy
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Содержит информацию вакансии
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 @Serializable
 data class VacancyResponse(
     val id: String? = null,
