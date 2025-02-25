5cc56c364c1ffd8d6c619a2b44a9d665323d58e0
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 18:05:35 +0300

    refactor: добавлено описание к ExperienceResponse

--- core/network/src/main/java/com/kire/network/dto/response/vacancy/ExperienceResponse.kt
+++ core/network/src/main/java/com/kire/network/dto/response/vacancy/ExperienceResponse.kt
@@ -2,6 +2,11 @@ package com.kire.network.dto.response.vacancy
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Содержит информацию о необходимом опыте работы
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 @Serializable
 data class ExperienceResponse(
     val previewText: String? = null,
