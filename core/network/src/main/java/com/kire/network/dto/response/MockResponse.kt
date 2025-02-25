aecd1d163f3d57e324c7a6e4dc433411cc15c5e3
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-25 18:09:45 +0300

    refactor: добавлено описание к MockResponse

--- core/network/src/main/java/com/kire/network/dto/response/MockResponse.kt
+++ core/network/src/main/java/com/kire/network/dto/response/MockResponse.kt
@@ -4,6 +4,11 @@ import com.kire.network.dto.response.offer.OfferResponse
 import com.kire.network.dto.response.vacancy.VacancyResponse
 import kotlinx.serialization.Serializable
 
+/**
+ * Содержит данные, присылаемые в mock
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 @Serializable
 data class MockResponse(
     val offers: List<OfferResponse>,
