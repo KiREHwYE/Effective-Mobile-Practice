d51f95b6699542f2057d436a3633bcd1c24245b9
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-27 23:03:26 +0300

    refactor: добавлены описания во FeatureModule

--- feature/vacancies/src/main/java/com/kire/vacancies/di/FeatureModule.kt
+++ feature/vacancies/src/main/java/com/kire/vacancies/di/FeatureModule.kt
@@ -10,9 +10,22 @@ import kotlinx.coroutines.CoroutineDispatcher
 
 /**
  * Отвечает за инжекцию зависимостей в рамках feature:vacancies
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
  */
 @Module
 class FeatureModule {
+
+    /**
+     * Отвечает за создание JobsRepository
+     *
+     * @param apiService для работы с API
+     * @param coroutineDispatcher поток выполнения корутины
+     *
+     * @see IJobsRepository
+     *
+     * @author Михаил Гонтарев (KiREHwYE)
+     */
     @Provides
     fun provideJobsRepository(apiService: IApiService, coroutineDispatcher: CoroutineDispatcher): IJobsRepository {
         return JobsRepository(apiService, coroutineDispatcher)
