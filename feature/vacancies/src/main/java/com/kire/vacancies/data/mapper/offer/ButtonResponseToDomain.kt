8f5405862923100327d20285285030420ee321f6
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-27 22:12:47 +0300

    refactor: добавлены описания к мапперам для Button и Offer

--- feature/vacancies/src/main/java/com/kire/vacancies/data/mapper/offer/ButtonResponseToDomain.kt
+++ feature/vacancies/src/main/java/com/kire/vacancies/data/mapper/offer/ButtonResponseToDomain.kt
@@ -1,8 +1,18 @@
 package com.kire.vacancies.data.mapper.offer
 
 import com.kire.network.dto.response.offer.ButtonResponse
+import com.kire.network.dto.response.offer.OfferResponse
 import com.kire.vacancies.domain.model.offer.ButtonDomain
+import com.kire.vacancies.domain.model.offer.OfferDomain
 
+/**
+ * Конвертирует ButtonResponse в ButtonDomain
+ *
+ * @see ButtonResponse
+ * @see ButtonDomain
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 fun ButtonResponse.toDomain() = ButtonDomain(
     text = this.text
 )
--- feature/vacancies/src/main/java/com/kire/vacancies/data/mapper/offer/OfferResponseToDomain.kt
+++ feature/vacancies/src/main/java/com/kire/vacancies/data/mapper/offer/OfferResponseToDomain.kt
@@ -3,6 +3,14 @@ package com.kire.vacancies.data.mapper.offer
 import com.kire.network.dto.response.offer.OfferResponse
 import com.kire.vacancies.domain.model.offer.OfferDomain
 
+/**
+ * Конвертирует OfferResponse в OfferDomain
+ *
+ * @see OfferResponse
+ * @see OfferDomain
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 fun OfferResponse.toDomain() = OfferDomain(
     id = this.id,
     title = this.title,
