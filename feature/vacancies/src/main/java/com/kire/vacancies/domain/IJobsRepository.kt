77ff5f6a712ce62fa8b68a47040f04dd949f3935
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-27 21:07:41 +0300

    feat: добавлен интерфейс репозитория для работы с предложениями для пользователя и вакансиями

---
+++ feature/vacancies/src/main/java/com/kire/vacancies/domain/IJobsRepository.kt
@@ -0,0 +1,10 @@
+package com.kire.vacancies.domain
+
+/**
+ * Репозиторий для работы с предложениями для пользователя и вакансиями
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
+interface IJobsRepository {
+
+}
