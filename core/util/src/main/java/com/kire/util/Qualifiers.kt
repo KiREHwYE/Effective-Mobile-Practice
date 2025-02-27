dd88be689fd1515694dd85562bb619977ac2ac9a
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-27 20:10:24 +0300

    refactor: добавлены описания к annotation классам IODispatcher и DefaultDispatcher

--- core/util/src/main/java/com/kire/util/Qualifiers.kt
+++ core/util/src/main/java/com/kire/util/Qualifiers.kt
@@ -2,10 +2,22 @@ package com.kire.util
 
 import javax.inject.Qualifier
 
+/**
+ * Аннотация Dagger для указания Dispatchers.IO
+ * при внедрении зависимостей
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class IODispatcher
 
+/**
+ * Аннотация Dagger для указания Dispatchers.Default
+ * при внедрении зависимостей
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class DefaultDispatcher
