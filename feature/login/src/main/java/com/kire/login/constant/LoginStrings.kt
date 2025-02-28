59779a8eab93534cb8e6b537d9a33d06fc931a0e
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-28 19:12:52 +0300

    feat: добавлен объект LoginStrings, содержащий строки, используемые в рамках экранов для логина

---
+++ feature/login/src/main/java/com/kire/login/constant/LoginStrings.kt
@@ -0,0 +1,20 @@
+package com.kire.login.constant
+
+/**
+ * Строки, используемые в рамках экранов для логина
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
+object LoginStrings {
+    const val LOGIN_TO_ACCOUNT = "Вход в личный кабинет"
+    const val JOB_SEARCH = "Поиск работы"
+    const val CONTINUE = "Продолжить"
+    const val LOGIN_VIA_PASSWORD = "Войти с паролем"
+    const val EMPLOYEES_SEARCH = "Поиск сотрудников"
+    const val POSTING_VACANCIES = "Размещение вакансий и доступ к базе резюме"
+    const val I_SEARCH_FOR_EMPLOYEES = "Я ищу сотрудников"
+
+    const val SENT_CODE_TO = "Отправили код на"
+    const val WRITE_CODE_SUGGESTION = "Напишите его, чтобы подтвердить, что это вы, а не кто-то другой входит в личный кабинет"
+    const val SUBMIT = "Подтвердить"
+}
