6220178096740afbe2734b09ec6b8ad1bfc18fb2
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-28 17:38:56 +0300

    feat: добавлен объект Dimens, содержащий информацию о паддингах, отступах и размерах в dp

---
+++ core/ui/src/main/java/com/kire/ui/Dimens.kt
@@ -0,0 +1,16 @@
+package com.kire.ui
+
+import androidx.compose.ui.unit.dp
+
+/**
+ * Содержит информацию о паддингах, отступах и размерах в dp
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
+object Dimens {
+    val HORIZONTAL_PAD_16 = 16.dp
+    val HORIZONTAL_PAD_8 = 8.dp
+
+    val VERTICAL_PAD_16 = 16.dp
+    val VERTICAL_PAD_10 = 16.dp
+}
