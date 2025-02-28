1f1016713f5b7b4fe2a8b96cf6dae9d83e51dd67
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-02-28 18:21:51 +0300

    feat: EffectiveMobileExtendedPracticeTheme подключена в MainActivity

--- app/src/main/java/com/kire/effectivemobilepractice/MainActivity.kt
+++ app/src/main/java/com/kire/effectivemobilepractice/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.kire.ui.EffectiveMobilePracticeTheme
+import com.kire.ui.EffectiveMobileExtendedPracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            com.kire.ui.EffectiveMobilePracticeTheme {
+            EffectiveMobileExtendedPracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
