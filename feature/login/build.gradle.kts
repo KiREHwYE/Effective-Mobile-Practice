9ef5bf486c8ca1069a74a9c75bc3a282fefb3521
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-01 10:22:29 +0300

    feat: добавил зависимость для Jetpack Navigation в gradle в feature:login

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -34,6 +34,9 @@ android {
 
 dependencies {
 
+    // Навигация
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
