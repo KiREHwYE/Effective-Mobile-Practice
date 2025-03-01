0a3961900a9ca6adda387efbb76897c71fed2c77
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-01 19:49:25 +0300

    refactor: добавлены описания

--- feature/jobs/src/main/java/com/kire/jobs/domain/model/offer/ButtonDomain.kt
+++ feature/jobs/src/main/java/com/kire/jobs/domain/model/offer/ButtonDomain.kt
@@ -1,5 +1,9 @@
 package com.kire.jobs.domain.model.offer
 
+/** Содержит информацию о тексте кнопки
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ * */
 data class ButtonDomain(
     val text: String? = null
 )
--- feature/jobs/src/main/java/com/kire/jobs/domain/model/offer/OfferDomain.kt
+++ feature/jobs/src/main/java/com/kire/jobs/domain/model/offer/OfferDomain.kt
@@ -1,5 +1,9 @@
 package com.kire.jobs.domain.model.offer
 
+/** Содержит информацию о предложении для пользователя
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ * */
 data class OfferDomain(
     val id: String? = null,
     val title: String? = null,
