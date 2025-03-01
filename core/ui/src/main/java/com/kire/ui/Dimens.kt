d2ae6154b7852d824b760f2b47b2b8e9f8ddadc9
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-01 12:22:46 +0300

    feat: добавлены новые значения паддингов в объект Dimens, а также значения закругления углов

--- core/ui/src/main/java/com/kire/ui/Dimens.kt
+++ core/ui/src/main/java/com/kire/ui/Dimens.kt
@@ -10,8 +10,16 @@ import androidx.compose.ui.unit.dp
 object Dimens {
     val HORIZONTAL_PAD_16 = 16.dp
     val HORIZONTAL_PAD_8 = 8.dp
+    val HORIZONTAL_PAD_24 = 24.dp
+    val HORIZONTAL_PAD_44 = 44.dp
 
+    val VERTICAL_PAD_7 = 7.dp
+    val VERTICAL_PAD_8 = 8.dp
     val VERTICAL_PAD_16 = 16.dp
     val VERTICAL_PAD_10 = 16.dp
+    val VERTICAL_PAD_11 = 11.dp
     val VERTICAL_PAD_24 = 24.dp
+
+    val ROUNDED_CORNERS_8 = 8.dp
+    val ROUNDED_CORNERS_50_PERCENT = 50
 }
