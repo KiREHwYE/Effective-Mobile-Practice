89e550b0b34abf42591346778394ec5eb34f59cf
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-02 16:51:17 +0300

    feat: добавлен объект для числовых констант (не dp)

---
+++ feature/jobs/src/main/java/com/kire/jobs/presentation/constant/Amounts.kt
@@ -0,0 +1,10 @@
+package com.kire.jobs.presentation.constant
+
+/**
+ * Для хранения числовых констант (не dp)
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
+object Amounts {
+    val VACANCIES_TO_SHOW = 3
+}
