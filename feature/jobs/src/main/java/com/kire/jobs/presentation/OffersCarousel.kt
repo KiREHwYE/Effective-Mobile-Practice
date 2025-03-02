2635ccf0ad507b3ff258ccbd64a4782e0bac5da5
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-02 13:04:33 +0300

    fix: исправлены некорректный размер поиска, отсутствие паддинга между кнопкой фильтра и поиском и иконки поиска

--- feature/jobs/src/main/java/com/kire/jobs/presentation/OffersCarousel.kt
+++ feature/jobs/src/main/java/com/kire/jobs/presentation/OffersCarousel.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -87,6 +89,7 @@ fun OffersCarousel(
 
             Tile(
                 modifier = Modifier
+                    .height(IntrinsicSize.Min)
                     .bounceClick {
                         // Переходим по ссылке
                         offer.link?.let { url ->
