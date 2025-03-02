e1aa58b99ce0b9ffa3afed41745f7e59d8b21090
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-02 13:45:23 +0300

    feat: создан MainScreen - главный экран приложения со списком вакансий и предложений. Пока что только с Topbar

--- feature/jobs/src/main/java/com/kire/jobs/presentation/MainScreen.kt
+++ feature/jobs/src/main/java/com/kire/jobs/presentation/MainScreen.kt
@@ -1,8 +1,16 @@
 package com.kire.jobs.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.kire.ui.Dimens.VERTICAL_PAD_32
+
 
 /**
  * Главный экран приложения со списком вакансий и предложений
@@ -16,5 +24,24 @@ fun MainScreen(
     jobsViewModel: JobsViewModel,
     modifier: Modifier = Modifier
 ) {
+    /** Список предложений для пользователя */
+    val offers by jobsViewModel.offers.collectAsStateWithLifecycle()
 
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(VERTICAL_PAD_32),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Topbar(
+            search = {
+                Search()
+            },
+            offersCarousel = {
+                OffersCarousel(
+                    offers = offers
+                )
+            }
+        )
+    }
 }
