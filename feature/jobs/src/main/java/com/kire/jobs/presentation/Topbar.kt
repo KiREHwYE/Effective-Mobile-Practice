9c6ee85983b5416e53ed10d02401fbfde39157b5
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-02 15:55:25 +0300

    feat: Topbar пере именован в TopbarWithOffers. В него внесен текст "Вакансии для вас"

--- feature/jobs/src/main/java/com/kire/jobs/presentation/Topbar.kt
+++ feature/jobs/src/main/java/com/kire/jobs/presentation/Topbar.kt
@@ -4,29 +4,54 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.kire.jobs.presentation.constant.JobsStrings.VACANCIES_FOR_YOU
 import com.kire.ui.Dimens.VERTICAL_PAD_16
+import com.kire.ui.Dimens.VERTICAL_PAD_32
+import com.kire.ui.theme.extendedColor
+import com.kire.ui.theme.extendedType
 
 /**
  * Топбар экрана с вакансиями и предложениями.
- * Содержит поиск и кнопку фильтра
+ * Содержит поиск, кнопку фильтра, карусель предложений
+ * и текст "Вакансии для вас"
+ *
+ * @param modifier модификатор
+ * @param search поиск и кнопка фильтра
+ * @param offersCarousel карусель плиток предложений для пользователся
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
  */
 @Composable
-fun Topbar(
+fun TopbarWithOffers(
     modifier: Modifier = Modifier,
     search: @Composable () -> Unit = {},
     offersCarousel: @Composable () -> Unit = {}
 ){
     Column(
-        modifier = modifier
+        modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight(),
-        verticalArrangement = Arrangement.spacedBy(VERTICAL_PAD_16),
-        horizontalAlignment = Alignment.Start
+        verticalArrangement = Arrangement.spacedBy(VERTICAL_PAD_32)
     ) {
-        search()
-        offersCarousel()
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            verticalArrangement = Arrangement.spacedBy(VERTICAL_PAD_16),
+            horizontalAlignment = Alignment.Start
+        ) {
+            search()
+            offersCarousel()
+        }
+
+        Text(
+            text = VACANCIES_FOR_YOU,
+            style = extendedType.title2,
+            color = extendedColor.white
+        )
     }
 }
