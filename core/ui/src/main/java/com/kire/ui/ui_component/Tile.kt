e7066609284d990c456581836f363109f8ab4e46
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-02 13:22:11 +0300

    fix: вызов параметра modifier перенесен ниже по иерархии Modifier, чтобы можно было переопределять размер компонента

--- core/ui/src/main/java/com/kire/ui/ui_component/Tile.kt
+++ core/ui/src/main/java/com/kire/ui/ui_component/Tile.kt
@@ -2,23 +2,17 @@ package com.kire.ui.ui_component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-
 import androidx.compose.runtime.Composable
-
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.Dp
-
 import com.kire.ui.Dimens.HORIZONTAL_PAD_16
 import com.kire.ui.Dimens.ROUNDED_CORNERS_8
 import com.kire.ui.Dimens.VERTICAL_PAD_16
@@ -48,9 +42,10 @@ fun Tile(
 ) {
 
     Column(
-        modifier = modifier
+        modifier = Modifier
             .wrapContentHeight()
             .fillMaxWidth()
+            .then(modifier)
             .clip(RoundedCornerShape(ROUNDED_CORNERS_8))
             .background(color = extendedColor.grey1)
             .padding(paddingValues),
