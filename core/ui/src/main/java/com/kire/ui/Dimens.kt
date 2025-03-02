1fb9c1741377a28cabcb9db3055e8a1ec31d886d
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-02 14:25:14 +0300

    feat: добавлен вертикальный паддинг на 4.dp refactor: паддинги упорядочены по возрастанию

--- core/ui/src/main/java/com/kire/ui/Dimens.kt
+++ core/ui/src/main/java/com/kire/ui/Dimens.kt
@@ -8,17 +8,18 @@ import androidx.compose.ui.unit.dp
  * @author Михаил Гонтарев (KiREHwYE)
  */
 object Dimens {
-    val HORIZONTAL_PAD_16 = 16.dp
     val HORIZONTAL_PAD_8 = 8.dp
+    val HORIZONTAL_PAD_16 = 16.dp
     val HORIZONTAL_PAD_24 = 24.dp
     val HORIZONTAL_PAD_44 = 44.dp
 
+    val VERTICAL_PAD_4 = 4.dp
     val VERTICAL_PAD_7 = 7.dp
     val VERTICAL_PAD_8 = 8.dp
-    val VERTICAL_PAD_16 = 16.dp
-    val VERTICAL_PAD_10 = 16.dp
     val VERTICAL_PAD_11 = 11.dp
+    val VERTICAL_PAD_10 = 16.dp
     val VERTICAL_PAD_14 = 14.dp
+    val VERTICAL_PAD_16 = 16.dp
     val VERTICAL_PAD_24 = 24.dp
     val VERTICAL_PAD_32 = 32.dp
 
