6e987a539d36081483c9282112f77e132ed06d2e
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-02 22:41:23 +0300

    feat: добавлен FavoriteStrings, содержащий строки, используемые на экране избранного

---
+++ feature/favourite_vacancies/src/main/java/com/kire/favourites/presentation/constant/FavoriteStrings.kt
@@ -0,0 +1,10 @@
+package com.kire.favourites.presentation.constant
+
+/**
+ * Строки, используемые на экране избранного
+ *
+ * @author Михаил Гонтарев (KiREHwYE)
+ */
+object FavoriteStrings {
+    const val FAVORITE = "Избранное"
+}
