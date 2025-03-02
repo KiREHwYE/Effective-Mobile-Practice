3bae5d2d1d94a2a2c507d8d0613bf4e7204478f3
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-02 13:21:03 +0300

    fix: все плитки подогнаны под один размер

--- feature/jobs/src/main/java/com/kire/jobs/presentation/OffersCarousel.kt
+++ feature/jobs/src/main/java/com/kire/jobs/presentation/OffersCarousel.kt
@@ -6,12 +6,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -19,14 +19,21 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import com.kire.jobs.presentation.constant.JobsDimens.OFFER_ICON_SIZE
+import com.kire.jobs.presentation.constant.JobsDimens.OFFER_WIDTH
 import com.kire.jobs.presentation.model.offer.Offer
 import com.kire.ui.Dimens.HORIZONTAL_PAD_8
 import com.kire.ui.Dimens.VERTICAL_PAD_10
@@ -76,6 +83,9 @@ fun OffersCarousel(
         }
     }
 
+    /** Чтобы выравнять все плитки под один размер */
+    var maxHeight by rememberSaveable { mutableStateOf(0) }
+
     LazyRow(
         modifier = modifier
             .fillMaxWidth()
@@ -89,7 +99,15 @@ fun OffersCarousel(
 
             Tile(
                 modifier = Modifier
-                    .height(IntrinsicSize.Min)
+                    .then(
+                        if (maxHeight == 0)
+                            Modifier.wrapContentHeight()
+                        else Modifier.height(with(LocalDensity.current) { maxHeight.toDp() })
+                    )
+                    .width(OFFER_WIDTH)
+                    .onGloballyPositioned { coordinates ->
+                        maxHeight = maxOf(maxHeight, coordinates.size.height)
+                    }
                     .bounceClick {
                         // Переходим по ссылке
                         offer.link?.let { url ->
@@ -127,10 +145,12 @@ fun OffersCarousel(
 
                 // Текст
                 Column(
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.Start
                 ) {
                     Text(
-                        text = offer.title ?: "",
+                        text = offer.title?.trim() ?: "",
                         maxLines = if (offer.button?.text.isNullOrEmpty()) 3 else 2,
                         overflow = TextOverflow.Ellipsis,
                         style = extendedType.title4,
